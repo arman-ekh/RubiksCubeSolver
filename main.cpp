f77a897968f34cd8849e1689aff465dde42387e5
Arman Ebrahimi Khoshdel <armanebrahimikhoshdel@gmail.com>
2026-09-02 01:20:07 +0330

    adding cube itself

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
+#include <cstdint>
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+struct Cube {
+    const uint64_t EdgesSolved = 0b01011'01010'01000'00110'00101'00100'00011'00010'00001'00000;
+    const uint64_t CornersSolved = 0b00111'00110'00101'00100'00011'00010'00001'00000;
+    uint64_t Edges ;
+    uint64_t Corners ;
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    bool isSolved() const {
+        return Edges == EdgesSolved && Corners == CornersSolved;
     }
 
+};
+
+int main() {
+
+
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
