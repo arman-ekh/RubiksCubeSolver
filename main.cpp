ce1fa6ace0a3c8c26468ef719c89b74175a6db00
Arman Ebrahimi Khoshdel <armanebrahimikhoshdel@gmail.com>
2026-09-03 01:24:01 +0330

    cube in progress

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <cstdint>
 #include <iostream>
 
-const uint64_t EdgesSolved = 0b01011'01010'01000'00110'00101'00100'00011'00010'00001'00000;
+const uint64_t EdgesSolved = 0b01011'01010'01001'01000'00111'00110'00101'00100'00011'00010'00001'00000;
 const uint64_t CornersSolved = 0b00111'00110'00101'00100'00011'00010'00001'00000;
 
 struct Cube {
@@ -18,12 +18,19 @@ struct Cube {
         this->Corners = Corners;
     }
 
+    void U() {
+        //Move edges
+        //move corners
+    }
+
 };
 
 int main() {
 
 
     std::cout << sizeof(Cube) << std::endl;
+
+    std::cout <<EdgesSolved << std::endl;
 
     return 0;
 }
