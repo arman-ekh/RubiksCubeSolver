ec4cd9f5e5822860c11314c3baaf22d2c2b5f3b2
Arman Ebrahimi Khoshdel <armanebrahimikhoshdel@gmail.com>
2026-09-14 22:56:11 +0330

    ida* in progress

--- include/IDA_star.h
+++ include/IDA_star.h
@@ -1,8 +1,19 @@
 #ifndef RUBIKSCUBESOLVER_IDA_STAR_H
 #define RUBIKSCUBESOLVER_IDA_STAR_H
+#include <atomic>
+
+#include "Cube.h"
 
 
 class IDA_star {
+    private:
+        long int max_heuristic;
+        bool is_solved;
+        long int node_count;
+    public:
+        IDA_star();
+        void search(Cube cube , int depth , uint8_t last_move ,  const std::atomic<bool>& global_solved);
+        bool isSolved();
 };
 
 
